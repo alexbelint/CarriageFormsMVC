5cd5fbf2fd42cc345e5d75691db8763230849128
BIT\a.rudich <rudichalex@yandex.ru>
2018-11-06 15:54:56 +0300

    кнопочку раскрасил

--- MVC_WebCargoRequestHandler/Controllers/ReportGeneratorController.cs
+++ MVC_WebCargoRequestHandler/Controllers/ReportGeneratorController.cs
@@ -2,10 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Linq.Dynamic;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace MVC_WebCargoRequestHandler.Controllers
 {
@@ -62,12 +69,39 @@ namespace MVC_WebCargoRequestHandler.Controllers
             {
                 foreach (var filter in filters)
                 {
-                    results = results.Where($"{filter.Column}.Contains(@0)", filter.Value == null ? "" : filter.Value);
+                    results = results.Where($"{filter.Column}.Contains(@0)", filter.Value ?? "");
                 }
             }
             
             var response = results.Select($"new ({filters.Single(x => x.Editing == true).Column} as value)").Distinct();
             return Json(response, JsonRequestBehavior.AllowGet);
         }
+        //[Authorize]
+        //[HttpPost]
+        //public ActionResult ExportToExcel(CargoForm cargoForm)
+        //{
+        //    var cargoFormsSearchedList = new DataTable("customTestTable");
+
+        //    var grid = new GridView();
+        //    grid.DataSource = cargoFormsSearchedList;
+        //    grid.DataBind();
+
+        //    Response.ClearContent();
+        //    Response.Buffer = true;
+        //    Response.AddHeader("content-disposition", "attachment; filename=CargoForms Report.xls");
+        //    Response.ContentType = "application/ms-excel";
+
+        //    Response.Charset = "";
+        //    StringWriter sw = new StringWriter();
+        //    HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+        //    grid.RenderControl(htw);
+
+        //    Response.Output.Write(sw.ToString());
+        //    Response.Flush();
+        //    Response.End();
+
+        //    return View(cargoForm);
+        //}
     }
 }
