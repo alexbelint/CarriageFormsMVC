6ef87338d24651db34cf746cab0f0944763fbe4e
BIT\a.rudich <rudichalex@yandex.ru>
2018-11-12 09:39:15 +0300

    success localize

--- MVC_WebCargoRequestHandler/App_Start/BundleConfig.cs
+++ MVC_WebCargoRequestHandler/App_Start/BundleConfig.cs
@@ -14,7 +14,7 @@ namespace MVC_WebCargoRequestHandler
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/select2").Include(
-                        "~/Scripts/select2.min.js"));
+                        "~/Scripts/select2.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -27,6 +27,7 @@ namespace MVC_WebCargoRequestHandler
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                       "~/Content/Sidebar.css",
                       "~/Content/select2.min.css"));
         }
     }
